3efe37b2ddc5b0c3d6faeba23b364b1fe03e4ee8
Jeesun Bari <jeesunbari2002@gmail.com>
2024-10-06 01:16:24 +0530

    Fix the Form issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,42 +1,56 @@
 "use client";
 import { Loader2, Mail, Phone } from "lucide-react";
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
-  } = useForm({
-    defaultValues: {
-      name: "",
-      email: "",
-      message: "",
-    },
-  });
 
-  const onSubmit = async (data) => {
-    const scriptURL = process.env.NEXT_PUBLIC_GOOGLE_SHEET_URL;
-    const formDataWithDate = {
-      ...data,
-      date: new Date().toLocaleString(),
-    };
-    const response = await fetch(scriptURL, {
-      method: "POST",
-      body: JSON.stringify(formDataWithDate),
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    Name: "",
+    Email: "",
+    Message: "",
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
-    if (response.ok) {
-      toast.success("Form submitted successfully!");
-      reset();
-    } else {
-      console.error("Error!", response);
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setIsSubmitting(true);
+      const scriptURL = process.env.NEXT_PUBLIC_GOOGLE_SHEET_URL;
+      // Convert form data to FormData
+      const formDataObj = new FormData();
+      formDataObj.append("Name", formData.Name);
+      formDataObj.append("Email", formData.Email);
+      formDataObj.append("Message", formData.Message);
+      formDataObj.append("Date", new Date().toLocaleString());
+      
+      const response = await fetch(scriptURL, {
+        method: "POST",
+        body: formDataObj,
+      });
+      
+      if(response.ok === true) {
+        toast.success("Form submitted successfully!");
+        setFormData({
+          Name: "",
+          Email: "",
+          Message: "",
+        });
+      }
+    } catch (error) {
+      console.error("Error!", error);
       toast.error("Form submission failed!");
+    } finally {
+      setIsSubmitting(false);
     }
   };
-  
+
   return (
     <div
       id="Contact"
@@ -63,97 +77,49 @@ const Contact = () => {
               receiving your issue ticket.
             </p>
             <p className="flex items-center mb-2 text-black dark:text-gray-200">
-              <Mail className="h-4 w-4 mr-2"/> help@gmail.com
+              <Mail className="h-4 w-4 mr-2" /> help@gmail.com
             </p>
             <p className="flex items-center mb-2 text-black dark:text-gray-200">
-              <Phone className="h-4 w-4 mr-2"/> 8016525211
+              <Phone className="h-4 w-4 mr-2" /> 8016525211
             </p>
           </div>
 
           <div className="w-full md:w-2/3">
             <form
-              onSubmit={handleSubmit(onSubmit)}
+              onSubmit={handleSubmit}
               className="w-full bg-white dark:bg-gray-800 p-8 rounded-md shadow-md"
             >
               <input
                 type="text"
+                name="Name"
+                value={formData.Name}
+                onChange={handleChange}
+                required
+                className="w-full px-4 py-3 border border-gray-300 dark:border-gray-700 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white"
                 placeholder="Enter your name"
-                {...register("name", {
-                  required: "Name is Required",
-                  maxLength: {
-                    value: 20,
-                    message: "Name cannot exceed 20 characters",
-                  },
-                })}
-                className={`w-full px-4 py-3 border ${
-                  errors.name
-                    ? "border-red-500"
-                    : "border-gray-300 dark:border-gray-700"
-                } rounded-md mb-4 focus:outline-none focus:ring-2 ${
-                  errors.name
-                    ? "focus:ring-red-500"
-                    : "focus:ring-black dark:focus:ring-white"
-                }`}
               />
-              {errors.name && (
-                <p className="text-red-500 text-sm mb-2">
-                  {errors.name.message}
-                </p>
-              )}
               <input
                 type="email"
+                name="Email"
                 placeholder="Enter your email"
-                {...register("email", {
-                  required: true,
-                  pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                    message: "Invalid Email Address",
-                  },
-                })}
-                className={`w-full px-4 py-3 border ${
-                  errors.email
-                    ? "border-red-500"
-                    : "border-gray-300 dark:border-gray-700"
-                } rounded-md mb-4 focus:outline-none focus:ring-2 ${
-                  errors.email
-                    ? "focus:ring-red-500"
-                    : "focus:ring-black dark:focus:ring-white"
-                }`}
+                required
+                value={formData.Email}
+                onChange={handleChange}
+                className="w-full px-4 py-3 border border-gray-300 dark:border-gray-700 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white"
               />
-              {errors.email && (
-                <p className="text-red-500 text-sm mb-2">
-                  {errors.email.message}
-                </p>
-              )}
               <textarea
                 rows="5"
                 placeholder="Your Message"
-                {...register("message", {
-                  required: "Message is required",
-                  maxLength: {
-                    value: 100,
-                    message: "Message should not exceed 100 characters",
-                  },
-                })}
-                className={`w-full px-4 py-3 border ${
-                  errors.message
-                    ? "border-red-500"
-                    : "border-gray-300 dark:border-gray-700"
-                } rounded-md mb-4 focus:outline-none focus:ring-2 ${
-                  errors.message
-                    ? "focus:ring-red-500"
-                    : "focus:ring-black dark:focus:ring-white"
-                }`}
+                name="Message"
+                value={formData.Message}
+                onChange={handleChange}
+                required
+                className="w-full px-4 py-3 border border-gray-300 dark:border-gray-700 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white"
               ></textarea>
-              {errors.message && (
-                <p className="text-red-500 text-sm mb-2">
-                  {errors.message.message}
-                </p>
-              )}
               <button
                 type="submit"
                 className="w-full py-3 bg-black text-white rounded-md flex justify-center items-center hover:bg-gray-800 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white"
-                disabled={isSubmitting}
+                desabled={isSubmitting}
               >
                 {isSubmitting ? (
                   <Loader2 className="w-4 h-4 animate-spin text-center" />
